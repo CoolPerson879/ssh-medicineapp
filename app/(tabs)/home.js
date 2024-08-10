57ba5f18d2d4ca85ac5f8178035c73943952cd74
CoolPerson879 <adityaykashyap@gmail.com>
2024-08-09 21:33:25 -0500

    yaaaa

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -4,163 +4,165 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect } from "@react-navigation/native";
 import { FocusAwareStatusBar } from "./_layout";
 import { LinearGradient } from 'expo-linear-gradient';
+import CustomLinearGradient from '../../components/CustomLinearGradient';
 
 
 export default function Home(){
-    const [totals, setTotals] = useState({
+  const [totals, setTotals] = useState({
+    Clinical: 0,
+    Extracurricular: 0,
+    Shadowing: 0,
+    Volunteer: 0,
+    Research: 0,
+  });
+  const [userName, setUserName] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("userData");
+        const userData = jsonValue != null ? JSON.parse(jsonValue) : null;
+        if (userData) {
+          setUserName(userData.name);
+        }
+      } catch (error) {
+        console.error("Error fetching user data", error);
+      }
+    };
+
+    const checkDarkModeStatus = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("isDark");
+        if (jsonValue != null) {
+          const darkMode = JSON.parse(jsonValue);
+          setIsDarkMode(darkMode);
+        } else {
+          console.log("no theme settings found!");
+        }
+      } catch (error) {
+        console.error("Error checking dark mode status", error);
+      }
+    };
+
+    checkDarkModeStatus();
+    fetchUserData();
+  }, []);
+
+  const calculateTotals = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("formData");
+      const formData = jsonValue != null ? JSON.parse(jsonValue) : [];
+
+      const newTotals = {
         Clinical: 0,
         Extracurricular: 0,
         Shadowing: 0,
         Volunteer: 0,
         Research: 0,
-      });
-      const [userName, setUserName] = useState("");
-      const [isDarkMode, setIsDarkMode] = useState(false);
-    
-      useEffect(() => {
-        const fetchUserData = async () => {
-          try {
-            const jsonValue = await AsyncStorage.getItem("userData");
-            const userData = jsonValue != null ? JSON.parse(jsonValue) : null;
-            if (userData) {
-              setUserName(userData.name);
-            }
-          } catch (error) {
-            console.error("Error fetching user data", error);
-          }
-        };
-    
-        const checkDarkModeStatus = async () => {
-          try {
-            const jsonValue = await AsyncStorage.getItem("isDark");
-            if (jsonValue != null) {
-              const darkMode = JSON.parse(jsonValue);
-              setIsDarkMode(darkMode);
-            } else {
-              console.log("no theme settings found!");
-            }
-          } catch (error) {
-            console.error("Error checking dark mode status", error);
-          }
-        };
-    
-        checkDarkModeStatus();
-        fetchUserData();
-      }, []);
-    
-      const calculateTotals = async () => {
-        try {
-          const jsonValue = await AsyncStorage.getItem("formData");
-          const formData = jsonValue != null ? JSON.parse(jsonValue) : [];
-    
-          const newTotals = {
-            Clinical: 0,
-            Extracurricular: 0,
-            Shadowing: 0,
-            Volunteer: 0,
-            Research: 0,
-          };
-    
-          formData.forEach((data) => {
-            if (data.number && data.category) {
-              newTotals[data.category] += parseFloat(data.number);
-            }
-          });
-    
-          setTotals(newTotals);
-        } catch (error) {
-          console.error("Error calculating totals", error);
-        }
       };
-    
-      useFocusEffect(
-        useCallback(() => {
-          calculateTotals();
-        }, [])
-      );
-    
-      const renderItem = (item) => (
-        <View
-          key={item.category}
-          style={isDarkMode ? styles.smallCardDark : styles.smallCard}
-        >
-          <Text style={styles.value}>{item.value}</Text>
-          <Text style={styles.label}>{item.category}</Text>
+
+      formData.forEach((data) => {
+        if (data.number && data.category) {
+          newTotals[data.category] += parseFloat(data.number);
+        }
+      });
+
+      setTotals(newTotals);
+    } catch (error) {
+      console.error("Error calculating totals", error);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      calculateTotals();
+    }, [])
+  );
+
+  const renderItem = (item) => (
+    <View
+      key={item.category}
+      style={isDarkMode ? styles.smallCardDark : styles.smallCard}
+    >
+      <Text style={styles.value}>{item.value}</Text>
+      <Text style={styles.label}>{item.category}</Text>
+    </View>
+  );
+
+  const data = [
+    { category: "Clinical", value: totals.Clinical },
+    { category: "Extracurricular", value: totals.Extracurricular },
+    { category: "Shadowing", value: totals.Shadowing },
+    { category: "Volunteer", value: totals.Volunteer },
+    { category: "Research", value: totals.Research },
+  ];
+
+  return (  
+    <>
+      <FocusAwareStatusBar hidden backgroundColor="#ecf0f1" />
+
+      {isDarkMode == false && (
+        <View style={styles.container}>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <View style={styles.bannerContainer}>
+            <Image
+              source={require("../../assets/banner.jpeg")} // Local banner image
+              style={styles.bannerImage}
+              resizeMode="cover"
+            />
+            <LinearGradient
+              colors={["rgba(0,0,0,0)", "#529bbb"]} // Transparent to desired color
+              style={styles.overlay}
+            />
+          </View>
+          <LinearGradient
+            colors={["#529bbb", "#eeaeca"]}
+            style={styles.backgroundGradient}
+          >
+            <View style={styles.centralContainer}>
+              <View style={styles.centralCard}>
+                <Text style={styles.greeting}>Hello {userName}</Text>
+                <View style={styles.row}>{data.map(renderItem)}</View>
+              </View>
+            </View>
+          </LinearGradient>
         </View>
-      );
-    
-      const data = [
-        { category: "Clinical", value: totals.Clinical },
-        { category: "Extracurricular", value: totals.Extracurricular },
-        { category: "Shadowing", value: totals.Shadowing },
-        { category: "Volunteer", value: totals.Volunteer },
-        { category: "Research", value: totals.Research },
-      ];
-    
-      return (
-        <>
-    
-          {isDarkMode == false && (
-            <View style={styles.container}>
-              <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-              />
-              <View style={styles.bannerContainer}>
-                <Image
-                  source={require("../../assets/banner.jpeg")} // Local banner image
-                  style={styles.bannerImage}
-                  resizeMode="cover"
-                />
-                {/* <LinearGradient
-                  colors={["rgba(0,0,0,0)", "#529bbb"]} // Transparent to desired color
-                  style={styles.overlay}
-                /> */}
+      )}
+      {isDarkMode && (
+        <View style={styles.container}>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <View style={styles.bannerContainer}>
+            <Image
+              source={require("../../assets/banner_wb.png")} // Local banner image
+              style={styles.bannerImage}
+              resizeMode="cover"
+            />
+          </View>
+          <LinearGradient
+            colors={["#181818", "#181818"]}
+            style={styles.backgroundGradient}
+          >
+            <View style={styles.centralContainer}>
+              <View style={styles.centralCardDark}>
+                <Text style={styles.greeting}>Hello {userName}</Text>
+                <View style={styles.row}>{data.map(renderItem)}</View>
               </View>
-              <LinearGradient
-                colors={["#529bbb", "#eeaeca"]}
-                style={styles.backgroundGradient}
-              >
-                <View style={styles.centralContainer}>
-                  <View style={styles.centralCard}>
-                    <Text style={styles.greeting}>Hello {userName}</Text>
-                    <View style={styles.row}>{data.map(renderItem)}</View>
-                  </View>
-                </View>
-              </LinearGradient>
             </View>
-          )}
-          {isDarkMode && (
-            <View style={styles.container}>
-              <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-              />
-              <View style={styles.bannerContainer}>
-                <Image
-                  source={require("../../assets/banner_wb.png")} // Local banner image
-                  style={styles.bannerImage}
-                  resizeMode="cover"
-                />
-              </View>
-              {/* <LinearGradient
-                colors={["#181818", "#181818"]}
-                style={styles.backgroundGradient}
-              > */}
-                <View style={styles.centralContainer}>
-                  <View style={styles.centralCardDark}>
-                    <Text style={styles.greeting}>Hello {userName}</Text>
-                    <View style={styles.row}>{data.map(renderItem)}</View>
-                  </View>
-                </View>
-              {/* </LinearGradient> */}
-            </View>
-          )}
-        </>
-      );
-}
+          </LinearGradient>
+        </View>
+      )}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -183,18 +185,31 @@ const styles = StyleSheet.create({
       borderBottomWidth: 6,
       borderTopWidth: 0,
     },
+    bannerImageLight: {
+      width: "100%",
+      height: "100%",
+      backgroundColor: "#529bbb",
+      borderColor: "#529bbb",
+      borderBottomWidth: 6,
+      borderTopWidth: 0,
+    },
+    gradient: {
+        ...StyleSheet.absoluteFillObject,
+      },
     overlay: {
       position: "absolute",
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
+      
     },
     backgroundGradient: {
       flex: 1,
       width: "100%",
       justifyContent: "center",
       alignItems: "center",
+      backgroundColor: "#529bbb",
     },
     centralContainer: {
       width: "90%",
